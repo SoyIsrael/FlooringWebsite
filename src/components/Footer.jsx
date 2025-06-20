1bb75ec99766a4c4afb4f1b32f050247cbe30db2
Israel Chavez <c.israel@wustl.edu>
2025-06-19 21:42:48 -0700

    test on vercel updating

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-100 text-center py-6 mt-20">
       <p className="text-sm text-gray-500">
-        © 2025 RaicesFlooring. All rights reserved.
+        © 2025 RaicesFlooring!. All rights reserved.
       </p>
     </footer>
   );
